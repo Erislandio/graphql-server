3962c24abebebe6e291038ad7a7ed71e7c1f9e20
Erislandio Soares <erislandiosoares21@gmail.com>
2018-12-02 11:57:16 -0200

    Adiçao do schema js

---
+++ db/Schema.js
@@ -0,0 +1,20 @@
+const {
+   GraphqlString,
+   GraphQLSchema,
+   GraphQLObjectType
+} = require('graphql')
+
+
+module.exports = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'RootQueryType',
+        fields: {
+            companhia: {
+                type: GraphqlString,
+                resolve() {
+                    return 'Aplle'
+                }
+            }
+        }
+    })
+})
