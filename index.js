59f04ee7ea029b35b9cdae95665fdb561a6a595e
Erislandio Soares <erislandiosoares21@gmail.com>
2018-12-02 11:46:37 -0200

    Adição do midlleware

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const schema = require('./db/Schema')
+
 
 const app = express();
-
 const port = 3000;
+
+// * midlleware
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true
+
+}))
+
 app.listen(port, () => {
     console.log(`servidor rodando na porta ${port}`)
 })
